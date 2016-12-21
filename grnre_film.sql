d294698f2ba365bd53386562aa500840736b419e
wangyu110 <1225974971@qq.com>
2016-12-21 11:29:01 +0800

    field

---
+++ grnre_film.sql
@@ -0,0 +1,17 @@
+create table field(
+id int auto_increment primary key,
+name varchar(128),
+country varchar(32),
+company varchar(128),
+film_producer varchar(32),
+money varchar(32)
+);
+insert into field values (null,'钢铁侠3','America','好莱坞 ',' ？？','$200,000,000');
+
+insert into field values (null,'寒战2','China_HongKong','天津联瑞影业有限公司','江志强','？？');
+
+insert into field values (null,'钢锯岭','America','熙颐影业','大卫·珀马特','5500万美元');
+
+insert into field values (null,'我不是潘金莲','China','北京耀莱影视文化传媒有限公司','周茂非','??');
+
+insert into field values (null,'那些年','China','群星瑞智艺能有限公司','柴智屏','??');
